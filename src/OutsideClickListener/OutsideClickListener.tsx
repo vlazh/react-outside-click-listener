e776b9cb01083f94b7194f9074440c392e694a35
Vladimir <vzh.inbox@gmail.com>
2020-07-30 21:20:04 +0600

    Add ignore prop

--- src/OutsideClickListener/OutsideClickListener.tsx
+++ src/OutsideClickListener/OutsideClickListener.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect, useRef } from 'react';
 
+export type HtmlTagSelectorMap = { [P in keyof JSX.IntrinsicElements]?: string | undefined };
+
 export interface OutsideClickListenerProps {
   /**
    * Useful for dropdowns, when dropdown box is not expanded set this prop to `true` to avoid unnesessary `onOutsideClick` event.
@@ -13,7 +15,11 @@ export interface OutsideClickListenerProps {
    * Clicks of outside of `topNode` is not catched.
    * Useful if you need avoid propogation click event from child node to `topNode`.
    */
-  topNode?: React.RefObject<Node> | Node | string | null;
+  topNode?: React.RefObject<Element> | Element | string | null;
+  /** Do not fire `onOutsideClick` event for these elements (and their children) */
+  ignore?: HtmlTagSelectorMap | ((node: Element) => boolean);
+  /** Same as `topNode` but for `ignore` */
+  ignoreTopNode?: React.RefObject<Element> | Element | string | null;
 }
 
 function setRef<T extends Node = Node>(
@@ -28,7 +34,18 @@ function setRef<T extends Node = Node>(
   }
 }
 
-function getNode(node: NonNullable<OutsideClickListenerProps['topNode']>): Node | null {
+function findUp(el: Element, map: HtmlTagSelectorMap, topNode?: Element | null): boolean {
+  const tag = el.nodeName.toLowerCase();
+  if (tag in map && (map[tag] == null || el.matches(map[tag]))) {
+    return true;
+  }
+  if (el === topNode || !el.parentElement) {
+    return false;
+  }
+  return findUp(el.parentElement, map, topNode);
+}
+
+function getNode(node: NonNullable<OutsideClickListenerProps['topNode']>): Element | null {
   if (typeof node === 'string') {
     const el = document.getElementById(node);
     if (!el) {
@@ -36,10 +53,10 @@ function getNode(node: NonNullable<OutsideClickListenerProps['topNode']>): Node 
     }
     return el;
   }
-  if (typeof node === 'object' && (node as React.RefObject<Node>).current instanceof Node) {
-    return (node as React.RefObject<Node>).current;
+  if (typeof node === 'object' && (node as React.RefObject<Element>).current instanceof Element) {
+    return (node as React.RefObject<Element>).current;
   }
-  return node as Node;
+  return node as Element;
 }
 
 /**
@@ -49,12 +66,15 @@ export default function OutsideClickListener({
   disabled = false,
   onOutsideClick,
   topNode,
+  ignore,
+  ignoreTopNode,
   children,
 }: OutsideClickListenerProps): JSX.Element {
-  const selfNodeRef = useRef<Node | null>(null);
+  const selfNodeRef = useRef<Element | null>(null);
+  const ignoreRef = useRef(ignore);
 
   const refHandler = React.useCallback(
-    (node: Node | null) => {
+    (node: Element | null) => {
       children.ref && setRef(children.ref, node); // pass ref to child ref handler if exists
       setRef(selfNodeRef, node);
     },
@@ -65,10 +85,23 @@ export default function OutsideClickListener({
     (event) => {
       if (disabled || !selfNodeRef.current) return;
 
-      const eventSourceNode = event.target as Node;
+      const eventSourceNode = event.target as Element;
       // Ignore clicks on the component itself.
       if (selfNodeRef.current.contains(eventSourceNode)) {
         return;
+      }
+
+      const { current: ignoreMapOrFn } = ignoreRef;
+      if (ignoreMapOrFn) {
+        const isIgnored =
+          typeof ignoreMapOrFn === 'object'
+            ? findUp(eventSourceNode, ignoreMapOrFn, ignoreTopNode ? getNode(ignoreTopNode) : null)
+            : ignoreMapOrFn(eventSourceNode);
+
+        if (isIgnored) {
+          // Just ignore and do nothing
+          return;
+        }
       }
 
       if (topNode) {
@@ -85,8 +118,12 @@ export default function OutsideClickListener({
       event.stopPropagation();
       onOutsideClick(event);
     },
-    [disabled, onOutsideClick, topNode]
+    [disabled, ignoreTopNode, onOutsideClick, topNode]
   );
+
+  useEffect(() => {
+    ignoreRef.current = ignore;
+  }, [ignore]);
 
   useEffect(() => {
     const node = topNode ? getNode(topNode) : document;
