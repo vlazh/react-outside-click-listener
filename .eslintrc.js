32b96bc2adedaaa4095fd07d15686e5c51ef4249
Vladimir <vzh.inbox@gmail.com>
2024-10-22 10:41:21 +0500

    Update deps and configs

--- .eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: require.resolve('@js-toolkit/configs/eslint/react'),
-};
---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  ...require('@js-toolkit/configs/eslint/common'),
+  ...require('@js-toolkit/configs/eslint/react'),
+];
